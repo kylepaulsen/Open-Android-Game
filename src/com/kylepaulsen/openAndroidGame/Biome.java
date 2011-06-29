94aa0b2e656980bfaad101d90572e4d9e5a0a7ba
kyle <krazykylep@gmail.com>
2011-06-28 17:50:25 -0700

    Added licence information to sourcefiles. Added the README and CPOYING file. Also disabled loading of the world map image... It's just too big.

--- src/com/kylepaulsen/openAndroidGame/Biome.java
+++ src/com/kylepaulsen/openAndroidGame/Biome.java
@@ -1,3 +1,22 @@
+/* Biome.java - Class for describing biomes.
+ * 
+ * Copyright © 2011 Kyle Paulsen
+ * Please see the file COPYING in this
+ * distribution for license terms.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ */
+
 package com.kylepaulsen.openAndroidGame;
 
 import android.content.Context;
@@ -34,19 +53,19 @@ public class Biome {
 		decodeOptions.inPreferredConfig = Bitmap.Config.ARGB_8888;
 		
 		//get the pixel color in the world map.
-		Bitmap world = BitmapFactory.decodeResource(con.getResources(), R.drawable.world_map, decodeOptions);
-		int landColor = world.getPixel(this.lonPxl, this.latPxl);
-		int red = Color.red(landColor);
-		int green = Color.green(landColor);
-		int blue = Color.blue(landColor);
+		//Bitmap world = BitmapFactory.decodeResource(con.getResources(), R.drawable.world_map, decodeOptions);
+		//int landColor = world.getPixel(this.lonPxl, this.latPxl);
+		//int red = Color.red(landColor);
+		//int green = Color.green(landColor);
+		//int blue = Color.blue(landColor);
 		
 		
 		Toast.makeText(con, "looking for pxl: "+this.lonPxl+", "+this.latPxl, Toast.LENGTH_LONG).show();
-		Toast.makeText(con, "Pxl rgb is: "+Color.red(landColor)+", "+Color.green(landColor)+", "+Color.blue(landColor), Toast.LENGTH_LONG).show();
+		//Toast.makeText(con, "Pxl rgb is: "+Color.red(landColor)+", "+Color.green(landColor)+", "+Color.blue(landColor), Toast.LENGTH_LONG).show();
 		
 		//get hsv
 		float hsv[] = new float[3];
-		Color.RGBToHSV(red, green, blue, hsv);
+		//Color.RGBToHSV(red, green, blue, hsv);
 		
 		//now if the h in hsv is between certain ranges, we have a biome.
 		//we could also use r, g, b, s or v for something.
--- src/com/kylepaulsen/openAndroidGame/LocationTask.java
+++ src/com/kylepaulsen/openAndroidGame/LocationTask.java
@@ -1,3 +1,22 @@
+/* LocationTask.java - Class for getting location data and doing things with it.
+ * 
+ * Copyright © 2011 Kyle Paulsen
+ * Please see the file COPYING in this
+ * distribution for license terms.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ */
+
 package com.kylepaulsen.openAndroidGame;
 
 import android.content.Context;
